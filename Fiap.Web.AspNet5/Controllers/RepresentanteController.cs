03d231d98504e309785d3942140531a796e1b295
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-05-09 20:56:51 -0300

    Criação do CRUD de Representantes

--- Fiap.Web.AspNet5/Controllers/RepresentanteController.cs
+++ Fiap.Web.AspNet5/Controllers/RepresentanteController.cs
@@ -7,36 +7,37 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Fiap.Web.AspNet5.Data;
 using Fiap.Web.AspNet5.Models;
+using Fiap.Web.AspNet5.Repository;
 
 namespace Fiap.Web.AspNet5.Controllers
 {
     public class RepresentanteController : Controller
     {
-        private readonly DataContext _context;
+        private readonly RepresentanteRepository representanteRepository;
 
         public RepresentanteController(DataContext context)
         {
-            _context = context;
+            representanteRepository = new RepresentanteRepository(context);
         }
 
         // GET: Representante
         public async Task<IActionResult> Index()
         {
-              return _context.Representantes != null ? 
-                          View(await _context.Representantes.ToListAsync()) :
-                          Problem("Entity set 'DataContext.Representantes'  is null.");
+            var listaRepresentantes = representanteRepository.FindAll();
+
+              return View(listaRepresentantes);
         }
 
         // GET: Representante/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public IActionResult Details(int? id)
         {
-            if (id == null || _context.Representantes == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var representanteModel = await _context.Representantes
-                .FirstOrDefaultAsync(m => m.RepresentanteId == id);
+            var representanteModel = representanteRepository.FindById((int)id);
+
             if (representanteModel == null)
             {
                 return NotFound();
@@ -51,44 +52,42 @@ namespace Fiap.Web.AspNet5.Controllers
             return View();
         }
 
-        // POST: Representante/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("RepresentanteId,NomeRepresentante")] RepresentanteModel representanteModel)
+        public IActionResult Create(RepresentanteModel representanteModel)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(representanteModel);
-                await _context.SaveChangesAsync();
+                representanteRepository.Insert(representanteModel);
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(representanteModel);
         }
 
         // GET: Representante/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult Edit(int? id)
         {
-            if (id == null || _context.Representantes == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var representanteModel = await _context.Representantes.FindAsync(id);
+            var representanteModel = representanteRepository.FindById((int)id);
+
             if (representanteModel == null)
             {
                 return NotFound();
             }
+
             return View(representanteModel);
         }
 
-        // POST: Representante/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("RepresentanteId,NomeRepresentante")] RepresentanteModel representanteModel)
+        public IActionResult Edit(int id, RepresentanteModel representanteModel)
         {
             if (id != representanteModel.RepresentanteId)
             {
@@ -97,37 +96,25 @@ namespace Fiap.Web.AspNet5.Controllers
 
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(representanteModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!RepresentanteModelExists(representanteModel.RepresentanteId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                
+                representanteRepository.Update(representanteModel);
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(representanteModel);
         }
 
         // GET: Representante/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public IActionResult Delete(int? id)
         {
-            if (id == null || _context.Representantes == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var representanteModel = await _context.Representantes
-                .FirstOrDefaultAsync(m => m.RepresentanteId == id);
+            var representanteModel = representanteRepository.FindById((int)id);
+
             if (representanteModel == null)
             {
                 return NotFound();
@@ -139,25 +126,23 @@ namespace Fiap.Web.AspNet5.Controllers
         // POST: Representante/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int? id)
         {
-            if (_context.Representantes == null)
+            if (id == null)
             {
                 return Problem("Entity set 'DataContext.Representantes'  is null.");
             }
-            var representanteModel = await _context.Representantes.FindAsync(id);
+
+            var representanteModel = representanteRepository.FindById((int)id);
+
             if (representanteModel != null)
             {
-                _context.Representantes.Remove(representanteModel);
+                representanteRepository.Delete(representanteModel);
             }
             
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+
         }
 
-        private bool RepresentanteModelExists(int id)
-        {
-          return (_context.Representantes?.Any(e => e.RepresentanteId == id)).GetValueOrDefault();
-        }
     }
 }
