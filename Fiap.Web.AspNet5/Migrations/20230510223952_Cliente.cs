0170cd15d7c4d2a556a5d0fcf5861c6a9f2676b3
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-05-10 19:44:49 -0300

    Criando Tabela Clientes

---
+++ Fiap.Web.AspNet5/Migrations/20230510223952_Cliente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiap.Web.AspNet5.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RepresentanteId = table.Column<int>(type: "int", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Sobrenome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Observacao = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                    table.ForeignKey(
+                        name: "FK_Clientes_REPRESENTANTE_RepresentanteId",
+                        column: x => x.RepresentanteId,
+                        principalTable: "REPRESENTANTE",
+                        principalColumn: "RepresentanteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_Nome",
+                table: "Clientes",
+                column: "Nome");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_RepresentanteId",
+                table: "Clientes",
+                column: "RepresentanteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
